33d3eba8e577b3c0b1145d2908f1c00deac9b7ef
JordyMail <144602804+JordyMail@users.noreply.github.com>
2025-04-05 02:12:17 +0700

    Create edituser.php

---
+++ edituser.php
@@ -0,0 +1,44 @@
+<html>
+<link rel="stylesheet" href="style.css">
+<body>
+<header>
+<blockquote>
+	<a href="index.php"><img src="image/logo.png"></a>
+</blockquote>
+</header>
+<blockquote>
+<div class="container">
+<form method="post"  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+	<h1>Edit Profile:</h1>
+	Full Name:<br><input type="text" name="name" placeholder="<?php echo $oName; ?>">
+	<span class="error" style="color: red; font-size: 0.8em;"><?php echo $nameErr;?></span><br><br>
+
+	User Name:<br><input type="text" name="uname" placeholder="<?php echo $oUserName; ?>">
+	<span class="error" style="color: red; font-size: 0.8em;"><?php echo $usernameErr;?></span><br><br>
+
+	New Password:<br><input type="password" name="upassword" placeholder="<?php echo $oPassword; ?>">
+	<span class="error" style="color: red; font-size: 0.8em;"><?php echo $passwordErr;?></span><br><br>
+
+	E-mail:<br><input type="text" name="email" placeholder="<?php echo $oEmail; ?>">
+	<span class="error" style="color: red; font-size: 0.8em;"><?php echo $emailErr;?></span><br><br>
+
+	Mobile Number:<br><input type="text" name="contact" placeholder="<?php echo $oPhone; ?>">
+	<span class="error" style="color: red; font-size: 0.8em;"><?php echo $contactErr;?></span><br><br>
+
+	<label>Gender:</label><br>
+	<input type="radio" name="gender" <?php if (isset($gender) && $gender == "Male") echo "checked";?> value="Male">Male
+	<input type="radio" name="gender" <?php if (isset($gender) && $gender == "Female") echo "checked";?> value="Female">Female
+	<span class="error" style="color: red; font-size: 0.8em;"><?php echo $genderErr;?></span><br><br>
+
+	<label>Address:</label><br>
+    <textarea name="address" cols="50" rows="5" placeholder="<?php echo $oAddress; ?>"></textarea>
+    <span class="error" style="color: red; font-size: 0.8em;"><?php echo $addressErr;?></span><br><br>
+	
+	<input class="button" type="submit" name="submitButton" value="Edit">
+	<input class="button" type="button" name="cancel" value="Cancel" onClick="window.location='index.php';" />
+</form>
+</div>
+</blockquote>
+</center>
+</body>
+</html>
